b34243584ac9f6aeeb896a2701882a7fd6738f61
Alfonso Jimena Ruiz <alfonsojimenaruiz@gmail.com>
2026-08-08 16:58:34 +0200

    Se crea el test_subsample en test_preprocessing.py.
    Se trata de un test un poco más complejo que los anteriores ya que se debe tener en cuenta la aleatoriedad de la función subsample.

--- word2vec_numpy/tests/test_preprocessing.py
+++ word2vec_numpy/tests/test_preprocessing.py
@@ -1,4 +1,5 @@
 import unittest
+import numpy as np
 from word2vec_numpy.word2vec.preprocessing import tokenize, build_vocab, subsample
 
 class TestPreprocessing(unittest.TestCase):
@@ -12,11 +13,25 @@ class TestPreprocessing(unittest.TestCase):
 
     def test_build_vocab(self):
         tokens = ['el', 'gato', 'duerme', 'tranquilamente', 'en', 'el', 'sofá', 'gato', 'gato', 'el', 'el']
+
         word2indx, idx2word, word_freqs = build_vocab(tokens, min_count=1)
 
         self.assertEqual(word2indx, {'el':0, 'gato':1, 'duerme':2, 'tranquilamente':3, 'en':4, 'sofá':5})
         self.assertEqual(idx2word, {0:'el', 1:'gato', 2:'duerme', 3:'tranquilamente', 4:'en', 5:'sofá'})
         self.assertEqual(word_freqs, {'el':4, "gato":3, 'duerme':1, 'tranquilamente':1, 'en':1, 'sofá':1})
+
+    def test_subsample(self):
+        np.random.seed(42) # Se fija una semilla para que el test sea reproducible
+        tokens = ['el', 'gato', 'duerme', 'tranquilamente', 'en', 'el', 'sofá', 'gato', 'gato', 'el', 'el']
+        word_freqs = {'el':4, "gato":3, 'duerme':1, 'tranquilamente':1, 'en':1, 'sofá':1}
+        threshold = 0.1 # Umbral alto adaptado a las palabras del corpus para que se descarte 'el' y se mantengan las demás palabras
+
+        result = subsample(tokens, word_freqs, threshold)
+        print(result)
+
+        self.assertTrue(result.count('el') < 4)  # él es muy frecuente y debería ser descartado en alguna ocasión.
+        self.assertTrue('duerme' in result)  # 'gato' es muy poco frecuente y debería permanecer
+        self.assertTrue('tranquilamente' in result)  # 'tranquilamente' es menos frecuente y debería permanecer
 
 if __name__ == '__main__':
     unittest.main()
