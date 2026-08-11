060eaaf0cabb4504ab5e438dc9407e292508597f
Alfonso Jimena Ruiz <alfonsojimenaruiz@gmail.com>
2026-08-11 17:50:05 +0200

    ligeros cambios

--- word2vec_numpy/word2vec/__init__.py
+++
@@ -1,9 +0,0 @@
-# Este archivo convierte la carpeta word2vec/ en un paquete importable.
-# Puedes dejarlo vacío, o (opcional, cuando ya tengas las funciones escritas)
-# reexportar aquí lo más usado para que los imports en main.py queden más limpios:
-#
-# from .preprocessing import tokenize, build_vocab, subsample
-# from .model import init_weights, forward_cbow, backward_cbow
-# from .train import train
-#
-# Por ahora no hace falta tocarlo.
