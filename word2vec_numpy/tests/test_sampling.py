67be88830b25b5513ed45533d297740626906560
Alfonso Jimena Ruiz <alfonsojimenaruiz@gmail.com>
2026-08-11 12:25:08 +0200

    Se desarrolla y se prueba exikstosamente la función test_generate_cbow_pairs en test_sampling.py
    Funciona correctamente, escrito íntegramente por mi.

--- word2vec_numpy/tests/test_sampling.py
+++ word2vec_numpy/tests/test_sampling.py
@@ -3,7 +3,18 @@ from word2vec_numpy.word2vec.sampling import generate_cbow_pairs, build_negative
 
 class TestSampling(unittest.TestCase):
     def test_generate_cbow_pairs(self):
-        raise NotImplementedError("Test not implemented yet.")
+        indiex = [10, 20, 30, 40, 50]
+        size_window = 2
+
+        result = generate_cbow_pairs(indiex, size_window)
+
+        self.assertEqual(result, [
+                                    ([20, 30], 10), 
+                                    ([10, 30, 40], 20), 
+                                    ([10, 20, 40, 50], 30), 
+                                    ([20, 30, 50], 40), 
+                                    ([30, 40], 50)
+                                ])
 
     def test_build_negative_sampling_table(self):
         raise NotImplementedError("Test not implemented yet.")
