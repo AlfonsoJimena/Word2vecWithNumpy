00f042e744ebe1cc400347836144bee72b963f93
Alfonso Jimena Ruiz <alfonsojimenaruiz@gmail.com>
2026-08-11 14:09:16 +0200

    test_build_negative_sampling_table en test_sampling.py completado con éxito, entiendo la lógica y el por qué de esta función.

--- word2vec_numpy/tests/test_sampling.py
+++ word2vec_numpy/tests/test_sampling.py
@@ -2,6 +2,7 @@ import unittest
 from word2vec_numpy.word2vec.sampling import generate_cbow_pairs, build_negative_sampling_table, sample_negatives
 
 class TestSampling(unittest.TestCase):
+
     def test_generate_cbow_pairs(self):
         indiex = [10, 20, 30, 40, 50]
         size_window = 2
@@ -17,7 +18,17 @@ class TestSampling(unittest.TestCase):
                                 ])
 
     def test_build_negative_sampling_table(self):
-        raise NotImplementedError("Test not implemented yet.")
+        word_freqs = {'el':100, 'perro': 10, 'guacamayo': 1}
+        word2idx = {'el': 0, 'perro': 1, 'guacamayo': 2}
+        table_size = 100000
+        power = 0.75
+
+
+        result = build_negative_sampling_table(word_freqs, word2idx, table_size, power)
+
+        self.assertEqual(len(result), table_size)
+        self.assertAlmostEqual((result == 0).sum() / table_size, 0.82, delta=0.1)
+
 
     def test_sample_negatives(self):
         raise NotImplementedError("Test not implemented yet.")
